5fd8224cb7750303e96f6833db52225b4e4b9014
LEE SEONHEE <leeseonhee17@gmail.com>
2024-03-04 16:16:37 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
 
-function App() {
+//``back tick
+// css를 써내려갈 부분
+const Father = styled.div`
+  display : flex;
+`;
+const Box = styled.div`
+background-color : ${(props) => props.bgColor};
+width : 100px;
+height: 100px;
+`;
+
+/* const Circle = styled.div`
+background-color : ${(props) => props.bgColor};
+width : 100px;
+height: 100px;
+border-radius: 50px;
+`; */
+
+//Box 속성 들고오기
+const Circle = styled(Box)`
+    border-radius: 50px;
+`;
+
+
+/* const BoxOne = styled.div`
+background-color : teal;
+width : 100px;
+height: 100px
+`;
+const BoxTwo = styled.div`
+background-color : tomato;
+width : 100px;
+height: 100px
+`;
+ */
+ const Text = styled.span`
+  color : black;
+`;
+
+ 
+function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        //<div style={{display :"flex"}}>
+        <Father>
+          {/*  <div style={{ backgroundColor:"teal",width :100 ,height:100 }}></div> */}
+          {/* <BoxOne>
+            <Text>Hellow</Text>
+          </BoxOne>
+          <BoxTwo /> */}
+          <Box bgColor="teal"/>
+          <Circle bgColor="grey"/>
+          <Text>black</Text>
+           {/* <div style={{ backgroundColor:"tomato",width :100 ,height:100 }}></div> */}
+        </Father>
+        // </div>
   );
 }
 
