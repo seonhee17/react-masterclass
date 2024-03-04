f0c7ee6022ba09ed77dcf94049086d817aa114ed
LEE SEONHEE <leeseonhee17@gmail.com>
2024-03-04 16:42:20 +0900

    #2.3

--- src/App.js
+++ src/App.js
@@ -5,56 +5,32 @@ import styled from "styled-components";
 const Father = styled.div`
   display : flex;
 `;
-const Box = styled.div`
-background-color : ${(props) => props.bgColor};
-width : 100px;
-height: 100px;
+
+const Btn = styled.button`
+  color:white;
+  background-color : tomato;
+  border : 0;
+  border-radius : 15px;
+`
+const Input  = styled.input.attrs({ required : true , minLength : 10 })`
+      background-color : tomato;
 `;
 
-/* const Circle = styled.div`
-background-color : ${(props) => props.bgColor};
-width : 100px;
-height: 100px;
-border-radius: 50px;
-`; */
-
-//Box 속성 들고오기
-const Circle = styled(Box)`
-    border-radius: 50px;
-`;
-
-
-/* const BoxOne = styled.div`
-background-color : teal;
-width : 100px;
-height: 100px
-`;
-const BoxTwo = styled.div`
-background-color : tomato;
-width : 100px;
-height: 100px
-`;
- */
- const Text = styled.span`
-  color : black;
-`;
-
- 
+//as 라는 props
+//링크로서 사용하고 싶다고 명시 할 수 있게 된다.
 function App(){
   return (
-        //<div style={{display :"flex"}}>
-        <Father>
-          {/*  <div style={{ backgroundColor:"teal",width :100 ,height:100 }}></div> */}
-          {/* <BoxOne>
-            <Text>Hellow</Text>
-          </BoxOne>
-          <BoxTwo /> */}
-          <Box bgColor="teal"/>
-          <Circle bgColor="grey"/>
-          <Text>black</Text>
-           {/* <div style={{ backgroundColor:"tomato",width :100 ,height:100 }}></div> */}
+        <Father as="header">
+          <Btn>Log in</Btn>
+          {/* a 태그로 변한다. 
+          html 태크 속성 사용가능 */}
+          <Input />
+          <Input />
+          <Input />
+          <Input />
+          <Input />
+          <Btn as="a" href="/" >Log in</Btn>
         </Father>
-        // </div>
   );
 }
 
