44547a93b17522d8c40f4c91ae69fbdfa410cc03
LEE SEONHEE <leeseonhee17@gmail.com>
2024-03-05 15:30:12 +0900

    #2.4 animation and Pseudo Selectors

--- src/App.js
+++ src/App.js
@@ -1,36 +1,62 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 //``back tick
 // css를 써내려갈 부분
-const Father = styled.div`
+const Wrapper = styled.div`
   display : flex;
 `;
 
-const Btn = styled.button`
-  color:white;
-  background-color : tomato;
-  border : 0;
-  border-radius : 15px;
-`
-const Input  = styled.input.attrs({ required : true , minLength : 10 })`
-      background-color : tomato;
+const rotateAnimation = keyframes`
+0% {
+  transform:rotate(0deg);
+  border-radius : 0px;
+}
+50%{
+  transform:rotate(360deg);
+  border-radius : 100px;
+}
+100%{
+  transform:rotate(0deg);
+  border-radius : 0px;
+  
+}` 
+;
+//${} 자바스크립트의 string interpolation 사용
+const Box = styled.div`
+  height:200px;
+  width:200px;
+  background-color: tomato;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  animation: ${rotateAnimation} 1s linear infinite;
+  //element target 처리 할 수 있다.
+  span{
+    font-size: 36px;
+    &:hover{
+    font-size: 40px;
+    }
+    &:active{
+      opacity: 0;
+    }
+  }
+  //span:hover과 동일 
+  
+ 
 `;
+
+
+
 
 //as 라는 props
 //링크로서 사용하고 싶다고 명시 할 수 있게 된다.
 function App(){
   return (
-        <Father as="header">
-          <Btn>Log in</Btn>
-          {/* a 태그로 변한다. 
-          html 태크 속성 사용가능 */}
-          <Input />
-          <Input />
-          <Input />
-          <Input />
-          <Input />
-          <Btn as="a" href="/" >Log in</Btn>
-        </Father>
+        <Wrapper as="header">
+          <Box>
+            <span>🫠</span>
+          </Box>
+        </Wrapper>
   );
 }
 
