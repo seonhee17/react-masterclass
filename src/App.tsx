18e0cec8f43049ce8c63ca9373a344d18848eff5
LEE SEONHEE <leeseonhee17@gmail.com>
2024-03-13 23:59:04 +0900

    셋팅

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+    color: ${(props) => props.theme.textColor};
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) => props.theme.backgroundColor};
+`;
+
+//as 라는 props
+//링크로서 사용하고 싶다고 명시 할 수 있게 된다.
+function App(){
+  return (
+        <Wrapper>
+            <Title>Hello</Title>
+        </Wrapper>
+  );
+}
+
+export default App;
